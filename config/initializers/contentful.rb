ff022304b6aaa22aa67f0d8a2dc7d61caa52efaf
Oare Arene <oare@fliqpay.com>
2021-03-15 00:10:39 +0100

    Setup contentful gem

---
+++ config/initializers/contentful.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+ContentfulRails.configure do |config|
+  config.access_token = ENV['CONTENTFUL_ACCESS_TOKEN']
+  config.space = ENV['CONTENTFUL_SPACE_ID']
+  config.environment = ENV['CONTENTFUL_ENVIRONMENT']
+end
